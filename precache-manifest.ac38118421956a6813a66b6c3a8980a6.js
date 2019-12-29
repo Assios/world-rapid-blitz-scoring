ebd9c1c0822e5c82cf5eba28d3c98c69f4a5618c
Asbjørn Steinskog <asbjorn@steinskog.me>
2019-12-29 22:28:13 +0100

    Updates

---
+++ precache-manifest.ac38118421956a6813a66b6c3a8980a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de738ceee07cb1e57699a574ff504ad3",
+    "url": "chessresults.assios.no/index.html"
+  },
+  {
+    "revision": "4d401efb0c24c372b9e3",
+    "url": "chessresults.assios.no/static/css/main.a47babb5.chunk.css"
+  },
+  {
+    "revision": "2071df97fa1f9a1d914a",
+    "url": "chessresults.assios.no/static/js/2.0f6d39c1.chunk.js"
+  },
+  {
+    "revision": "4d401efb0c24c372b9e3",
+    "url": "chessresults.assios.no/static/js/main.02bb8036.chunk.js"
+  },
+  {
+    "revision": "d62346a7232905cd3fed",
+    "url": "chessresults.assios.no/static/js/runtime~main.fe670c1e.js"
+  }
+]);
